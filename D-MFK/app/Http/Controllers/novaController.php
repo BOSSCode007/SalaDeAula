6e33e6032c42df3c394bb9e95308b99e1e032368
Aluno <pedrocorrea3145@gmail.com>
2025-05-06 10:47:26 -0300

    Rota e Controle criada

---
+++ D-MFK/app/Http/Controllers/novaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class novaController extends Controller
+{
+    public function detalhes (){
+        return view ('detalhes'); //mudar
+    }
+}
--- D-MFK/routes/web.php
+++ D-MFK/routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\novaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test', [TestController:: class, 'test'])->name('test');
+Route::get('/detalhes', [novaController::class, 'detalhes'])->name('detalhes');
